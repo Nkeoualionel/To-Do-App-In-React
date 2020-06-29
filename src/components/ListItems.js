6e6bcd8971814c565002479ec932e26dffc7f1b4
Lionel <lnkeoua@ludexgames.com>
2020-06-29 19:22:08 +0000

    create item for liste

---
+++ src/components/ListItems.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ListItem from '../ListItem.css'
+
+function ListItems(props) {
+  return (
+    <>
+        <h4>Liste Item.js</h4>
+    </>
+  );
+}
+
+
+export default ListItems;
