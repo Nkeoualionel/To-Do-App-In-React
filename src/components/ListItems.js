a9531697c9c93c5287f487a6065e2d7c4c2d6bf3
Lionel <lnkeoua@ludexgames.com>
2020-07-01 20:01:45 +0000

    added listitems

--- src/components/ListItems.js
+++ src/components/ListItems.js
@@ -2,11 +2,20 @@ import React from 'react';
 import ListItem from '../ListItem.css'
 
 function ListItems(props) {
-  return (
-    <>
-        <h4>Liste Item.js</h4>
-    </>
-  );
+
+          
+
+         const items = props.items
+         const listItems = items.map(item => {
+
+          return <div className="list" key="item.key">
+                      <p>{item.text}</p>
+                 </div>
+
+         })
+         return (
+           <ul>{listItems}</ul>
+         );
 }
 
 
