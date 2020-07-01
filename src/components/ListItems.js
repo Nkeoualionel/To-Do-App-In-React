9aeadd41e9cfb4958970d7b4fe0d8db624e2d7a6
Lionel <lnkeoua@ludexgames.com>
2020-07-01 20:14:43 +0000

    implemented fontaweme

--- src/components/ListItems.js
+++ src/components/ListItems.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ListItem from '../ListItem.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 function ListItems(props) {
 
@@ -10,6 +11,9 @@ function ListItems(props) {
 
           return <div className="list" key="item.key">
                       <p>{item.text}</p>
+                      <span>
+                        <FontAwesomeIcon className="faicons" icon='trash'/>
+                      </span>
                  </div>
 
          })
